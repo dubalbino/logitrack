8c94480efa07923c57ac7182406bda37dc330984
dubalbino <ebsbalbino@gmail.com>
2025-12-01 16:03:16 -0300

    fix(deploy): hardcode supabase keys to resolve runtime error

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,11 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
-
-if (!supabaseUrl || !supabaseAnonKey) {
-  const availableVars = Object.keys(import.meta.env).join(', ');
-  throw new Error(`As variáveis de ambiente VITE_SUPABASE_URL e VITE_SUPABASE_ANON_KEY não foram encontradas. Variáveis disponíveis: ${availableVars}`);
-}
+const supabaseUrl = "https://ycnnjxkgshodzdmaswjq.supabase.co"
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inljbm5qeGtnc2hvZHpkbWFzd2pxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQxMjA0OTMsImV4cCI6MjA3OTY5NjQ5M30.ow71AesiCmMUVRRfBw2NCkRu3H9bdOSA95HmXgL6TAY"
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
